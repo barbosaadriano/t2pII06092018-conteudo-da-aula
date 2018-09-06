0419b399bac2c6993af882738ed3d220fb689f9a
Adriano Barbosa <b.adrianobarbosa@gmail.com>
2018-09-06 20:16:52 -0300

    Commentários

--- src/br/com/adrianob/AppExemplo.java
+++ src/br/com/adrianob/AppExemplo.java
@@ -1,20 +1,17 @@
+//nome do pacote
 package br.com.adrianob;
-
+//importações
 import br.com.adrianob.controller.ClienteController;
 import br.com.adrianob.service.Conexao;
-
-/**
- *
- * @author drink
- */
+//classe principal
 public class AppExemplo {
-
-    /**
-     * @param args the command line arguments
-     */
+// método principal, inicia o programa
     public static void main(String[] args) {
+        //instancia da classe Conexao
         Conexao cnx = Conexao.getInstance();
+        //Declaração e atribuição da classe ClienteController
         ClienteController cc = new ClienteController(cnx.getConn());
+        //Execução da classe Controller criada anteriormente
         cc.abrirLista();
     }
     
