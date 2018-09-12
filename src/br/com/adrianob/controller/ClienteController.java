a36abc013d5a7bd4844b30a6363ec8e976979d88
Erica <ericaborges97@hotmail.com>
2018-09-12 14:09:56 -0300

    exercicio todo comentado

--- src/br/com/adrianob/controller/ClienteController.java
+++ src/br/com/adrianob/controller/ClienteController.java
@@ -1,5 +1,7 @@
+//nome do pacote
 package br.com.adrianob.controller;
 
+//importações
 import br.com.adrianob.dao.DaoCliente;
 import br.com.adrianob.model.Cliente;
 import br.com.adrianob.view.ClienteEditView;
@@ -16,105 +18,159 @@ import javax.swing.JOptionPane;
  *
  * @author drink
  */
+//CLASSE CLIENTECONTROLLER COM INTERFACE DE ACTIONLISTENER
 public class ClienteController implements ActionListener {
 
+    //DECLARAÇÃO DE VARIAVEIS DE ESCOPO DE CLASSE
     private ClienteListView telaLista;
     private ClienteEditView edicao;
     private DaoCliente dc;
 
+    //CONSTRUTOR DA CLASSE COM INJEÇÃO DE DEPEDENCI
     public ClienteController(Connection conn) {
         dc = new DaoCliente(conn);
     }
 
+    //METODO DA INTERFACE DE ACTIONLISTENER	
     @Override
     public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()) {
+        
+        //CONDIÇÃO PARA COMANDO PASSADO PARA O ACTIONPERFORMED
+        switch (e.getActionCommand()){
+            //PEDIR O COMANDO DE LISTAR, ACIONA O METODO DE LISTAGEM
             case "cliente.listar":
                 listar();
                 break;
+            //PEDIR O COMANDO DE NOVO, ACIONA O METODO DE NOVO	
             case "cliente.novo":
                 novo();
                 break;
+            //PEDIR O COMANDO DE SALVAR, ACIONA O METODO SALVAR	
             case "cliente.salvar":
                 salvar();
                 break;
+             //PEDIR O COMANDO DE EDITAR, ACIONA O MÉTODO DE EDITAR
             case "cliente.editar":
                 editar();
                 break;
+            //PEDIR O COMANDO DE REMOVER, ACIONA O MÉTODO DE REMOVER
             case "cliente.remover":
                 remover();
                 break;
         }
     }
-
+    
+    //método para abrir tela de edição
     public void abrirEdicao(Cliente c) {
-
+        
+        //variável (escopo de classe) recebe um objeto ClientEditView.
         edicao = new ClienteEditView(this, c);
+        //Aponta que apenas essa tela pode ser acessada enquanto existir.
         edicao.setModal(true);
+        //Aponta que a tela ficará visível.
         edicao.setVisible(true);
     }
 
+    //Método para abrir tela de Listagem
     public void abrirLista() {
+        //se a variável (escopo de classe) for nula...
         if (this.telaLista == null) {
+            //Faz com que a variável receba um objeto do tipo ClienteListView.
             this.telaLista = new ClienteListView(this);
+            //Aponta que a tela criada fechara o sistema completo quando for fechada
             this.telaLista.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         }
+        //mostra que a a tela ficará visível
         this.telaLista.setVisible(true);
     }
-
+    
+   //metodo para listar
     private void listar() {
+        //Cria um ArrayList de objeto Cliente 
         ArrayList<Cliente> listarCliente = new ArrayList<>();
+        //se o input de pesquisa não estiver vazio
         if (!telaLista.getTxtPesquisa().getText().isEmpty()) {
+            //Cria um novo objeto cliente
             Cliente c = new Cliente();
+            //Aciona o método para mudar a propriedade nome passando o input de pesquisa
             c.setNome(telaLista.getTxtPesquisa().getText());
+            //Retorna o cliente utilizando o método do DaoCliente
             listarCliente = this.dc.listarCliente(c);
-        } else {
+        } else {  //se estiver vazio o input de pesquisa, retorna todos clientes
             listarCliente = this.dc.listarCliente(null);
         }
+        //Atualiza o modelo da tabela para a consulta
         telaLista.getTabela().setModel(new ClienteTableModel(listarCliente));
     }
 
+    //metodo para um cadastro novo
     private void novo() {
+        //Abre a tela de edição para o cadastro de um novo cliente
         this.abrirEdicao(new Cliente());
     }
 
+    //metodo para salvar
     private void salvar() {
+        //se a tela de edição não esteja inicializada
         if (edicao != null) {
+            //Objeto cliente recebe os dados da tela de edição
             Cliente cli = edicao.getDadosCliente();
+            //Se o objeto cliente não estiver nulo
             if (cli != null) {
+                //Salva o cliente utilizando o DAO
                 dc.salvarCliente(cli);
                 listar();
+                //Tela de edição é finalizada
                 edicao = null;
             }
         }
     }
 
+    //metodo editar
     private void editar() {
+        //Abre a tela de edição usando o cliente selecionado na tabela.
         abrirEdicao(getClientSelecionado());
     }
 
+    //Método para pegar o cliente selecionado
     private Cliente getClientSelecionado() {
+        //se a tela de listagem esteja inicializada
         if (this.telaLista != null) {
+            //se a contagem de linhas selecionadas seja igual à 1
             if (telaLista.getTabela().getSelectedRowCount() == 1) {
+                 //Variavel recebe valor de index da linha selecionada
                 int idx = telaLista.getTabela().getSelectedRow();
+                //Variavel recebe o id da linha selecionada
                 int codigo = (int) telaLista.getTabela().getValueAt(idx, 0);
+                //Cria um novo objeto cliente
                 Cliente c = new Cliente();
+                //Passa o valor id para o objeto cliente
                 c.setCodigo(codigo);
+                //Um objeto ArrayList recebe o retorno pela consulta deste id
                 ArrayList<Cliente> lc = dc.listarCliente(c);
+                //Se o retorno não for vazio
                 if (!lc.isEmpty()) {
+                    //retorna o resultado
                     return lc.get(0);
                 }
             }
         }
+        //senao retorna nulo
         return null;
     }
 
+    //metodo remover
     private void remover() {
+        //se confirmar a remoção
         if (JOptionPane.showConfirmDialog(edicao, "Deseja Remover?", "confirmação", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE)
                 == JOptionPane.YES_OPTION) {
+            //Objeto cliente recebe o cliente selecionado
             Cliente cli = getClientSelecionado();
+            //se nao for nulo
             if (cli != null) {
+                ////Ativa o método de remoção do DAO
                 dc.removerCliente(cli);
+                //lista novamente
                 listar();
             }
         }
--- src/br/com/adrianob/service/Conexao.java
+++ src/br/com/adrianob/service/Conexao.java
@@ -1,5 +1,7 @@
+//nome do pacote
 package br.com.adrianob.service;
 
+//importações
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,43 +11,63 @@ import java.util.Properties;
  *
  * @author drink
  */
+//classe conexao
 public class Conexao {
 
+    //Variável de escopo de classe 
     private static Conexao instance = null;
 
-
+    //Método de retorno de Connection (com objetivo de cria-la)
     private static Connection createConnection() {
+        //Cria um objeto Properties
         Properties config = new Properties();
+        //Insere valores de configuração
         config.put("user", "devel");
+        //Insere valores de configuração
         config.put("password", "developer");
+        //Cria uma Connection nula
         Connection conn = null;
+        //O código a seguir pode gerar exceptions, então está circundado com tryCatch
         try {
+            //Passa o driver de conexão
             Class.forName("com.mysql.jdbc.Driver");
-
+            
+            //A Connection tenta conectar utilizando o driver e as configurações passadas
             conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/singleton",
                     config
             );
 
+        //Caso der uma exception, printa a mensagem de erro
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
         }
+        //retorna conexão 
         return conn;
     }
+    //Variável de escopo de classe
     private Connection conn;
 
+    //construtor da classe
     private Conexao() {
+        //Variável de escopo de classe recebe o retorno do método createConnection
         this.conn = Conexao.createConnection();
     }
 
+    //Método Getter da instance
     public static Conexao getInstance() {
+         //Se a instância estiver nula..
         if (Conexao.instance == null) {
+            //A instância recebe uma nova Conexao
             Conexao.instance = new Conexao();
         }
+        //retorna a instância da classe Conexao
         return Conexao.instance;
     }
-
+    
+    //Método Getter da variável conn
     public Connection getConn() {
+        //retorna a Connection
         return conn;
     }
 
